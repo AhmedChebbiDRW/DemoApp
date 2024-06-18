aa18b5ae104c31d77fe0fb751d3358255fdf6e85
Chebbi Ahmed <ahmed.chebbi@digitalroguewave.com>
2024-06-18 10:36:34 +0100

    fix(resources): build staging app

--- src/core/i18n/resources.ts
+++ src/core/i18n/resources.ts
@@ -1,5 +1,70 @@
-import ar from '@/translations/ar.json';
-import en from '@/translations/en.json';
+const ar = {
+  onboarding: {
+    message: 'مرحبا بكم في موقع تطبيق obytes',
+  },
+  settings: {
+    about: 'حول التطبيق ',
+    app_name: 'اسم التطبيق',
+    arabic: 'عربي',
+    english: 'إنجليزي',
+    generale: 'عام',
+    github: 'جيثب',
+    language: 'لغة',
+    links: 'الروابط',
+    logout: 'تسجيل خروج',
+    more: 'أكثر',
+    privacy: 'سياسة الخصوصية',
+    rate: 'تقييم',
+    share: 'شارك',
+    support: 'الدعم',
+    support_us: 'ادعمنا',
+    terms: 'شروط الخدمة',
+    theme: {
+      dark: 'مظلم',
+      light: 'خفيفة',
+      system: 'System',
+      title: 'سمة',
+    },
+    title: 'إعدادات',
+    version: 'إصدار',
+    website: 'موقع الكتروني',
+  },
+  welcome: 'test arabic',
+};
+
+const en = {
+  onboarding: {
+    message: 'Welcome to obytes app site',
+  },
+  settings: {
+    about: 'About',
+    app_name: 'App Name',
+    arabic: 'Arabic',
+    english: 'English',
+    generale: 'General',
+    github: 'Github',
+    language: 'Language',
+    links: 'Links',
+    logout: 'Logout',
+    more: 'More',
+    privacy: 'Privacy Policy',
+    rate: 'Rate',
+    share: 'Share',
+    support: 'Support',
+    support_us: 'Support Us',
+    terms: 'Terms of Service',
+    theme: {
+      dark: 'Dark',
+      light: 'Light',
+      system: 'System',
+      title: 'Theme',
+    },
+    title: 'Settings',
+    version: 'Version',
+    website: 'Website',
+  },
+  welcome: 'Welcome to obytes app site',
+};
 
 export const resources = {
   en: {
